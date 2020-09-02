1481199d16cf587d8129ee7d3a215c6e0ec8f0e9
acarril <acarril@princeton.edu>
2020-09-02 16:00:13 -0400

    add comments and other small fixes

--- 01_live_demand.jl
+++ 01_live_demand.jl
@@ -39,16 +39,17 @@ begin
 	ylabel!("Price (per pizza slice)")
 end
 
-# ╔═╡ d65d5724-ed53-11ea-2573-71e016c67613
+# ╔═╡ b49adfa6-ed55-11ea-3cb2-73ec7ecfcea5
 md"That's it!
 
-When you look at a demand curve, remember it represents _total_ quantity demanded as a function of price.
+When you look at a demand curve, remember it represents _total_ quantity demanded as a function of price."
 
-## What about demand curves as functions?
+# ╔═╡ d65d5724-ed53-11ea-2573-71e016c67613
+md"## What about demand curves as functions?
 
 Defining a demand curve as a continuous, smooth function such as
 
-$$P(Q) = 15 - \frac{Q}{2}$$
+$$p(q) = 15 - \frac{q}{2}$$
 
 is done basically out of convenience, and it is likely an approximation of the real demand.
 
@@ -66,15 +67,17 @@ For example, suppose I approximate our (empirical) demand for pizza slices with 
 
 $$q(p) = \frac{20}{p},$$
 
-and then compare the approximation and the real one:
-"
+and then compare the approximation to the real one:"
 
 # ╔═╡ 8b7f54e0-ed52-11ea-1d88-d9dfbe4758bb
+# Define functional form for quantity 'q' as a function input 'x' (it's going to be price later)
 q_fun(x) = 20/x;
 
 # ╔═╡ 1f705464-ed51-11ea-01a3-0352ad446169
+# Plot empirical demand curve (same as above) and overlay (with the '!' modifier) a plot of the approximated demand curve
 begin
 	plot(q, p, label = "Empirical demand")
+	# Note that the 'x' axis is the result of the function we defined above, and the '.' modifier applies the function to each element of the passed array ('p')
 	plot!(q_fun.(p), p, label = "Functional approximation")
 end
 
@@ -88,9 +91,10 @@ Can you figure out why is the approximated demand curve shorter than the empiric
 # ╟─558e8cd6-ed4d-11ea-3573-df3ded1e403e
 # ╟─c67a4320-ed4d-11ea-237a-d1fb457661d1
 # ╠═f99509b0-ed46-11ea-2cd3-773b6df84b6a
-# ╟─2bd8305a-ed47-11ea-1fc6-dfafdfce9c82
-# ╠═4f7bf618-ed47-11ea-3994-f5a527501359
+# ╠═2bd8305a-ed47-11ea-1fc6-dfafdfce9c82
+# ╟─4f7bf618-ed47-11ea-3994-f5a527501359
+# ╟─b49adfa6-ed55-11ea-3cb2-73ec7ecfcea5
 # ╟─d65d5724-ed53-11ea-2573-71e016c67613
-# ╟─8b7f54e0-ed52-11ea-1d88-d9dfbe4758bb
-# ╟─1f705464-ed51-11ea-01a3-0352ad446169
+# ╠═8b7f54e0-ed52-11ea-1d88-d9dfbe4758bb
+# ╠═1f705464-ed51-11ea-01a3-0352ad446169
 # ╟─d6eafc22-ed54-11ea-106e-551a9c389c5e
