d19b77d019ff0f1a2660ef8b5d59ecfe1bc72a88
acarril <acarril@princeton.edu>
2020-09-10 08:30:11 -0400

    update to version 2.0

--- boston_mechanism.jl
+++ boston_mechanism.jl
@@ -1,104 +1,150 @@
+# Load libraries
 using Random
+using StatsBase
 Random.seed!(511)
 
-γ = (
-    rPrefs = (3, 4, 1, 2, 6, 0, 5),
-    rDates = (2, 3, 1)
-)
-τ = (
-    rPrefs = (1, 3, 4, 0, 5, 6, 2),
-    rDates = (3, 2, 1)
-)
-ι = (
-    rPrefs = (6, 0, 5, 1, 3, 4, 2),
-    rDates = (3, 2, 1)
-)
-ϕ = (
-    rPrefs = (0, 4, 5, 1, 3, 2, 6),
-    rDates = (3, 2, 1)
-)
-δ = (
-    rPrefs = (6, 1, 2, 3, 5, 4, 0),
-    rDates = (3, 2, 1)
-)
-ω = (
-    rPrefs = (0, 4, 6, 5, 1, 3, 2),
-    rDates = (3, 2, 1)
-)
-θ = (
-    rPrefs = (1, 6, 4, 3, 5, 2, 0),
-    rDates = (1, 2, 3)
-)
-λ = (
-    rPrefs = (6, 5, 1, 4, 2, 3, 0),
-    rDates = (1, 2, 3)
-)
-
-I0 = (
-    γ = γ,
-    τ = τ,
-    ι = ι,
-    ϕ = ϕ,
-    δ = δ,
-    ω = ω,
-    θ = θ,
-    λ = λ
-)
-C0 = (1:6)
-
-# Define function for Boston algorithm
-function boston_mechanism(I, C, rank)
+function boston_mechanism(G, C, P)
+    print("\n Starting new call....\n")
     A = Dict()
-    mech_step = 0
-    while length(A) < length(C)
-    # for i in 1:1
-        mech_step += 1
-        for option in C
-            # Skip option if already assigned
-            if option in values(A)
-                continue
+    unassigned = [i for i in G]
+    step = 0
+    while length(A) < length(G)
+        step += 1
+        println("step: $step")
+        for (option, capacity) in enumerate(C)
+            print("option: $(option - 1), ")
+            # Check capacity of option
+            try
+                global n_assigned_option = sum(i -> i == option - 1, values(A))
+                print("N_before:$n_assigned_option, ")
+                # Skip this option if at capacity
+                if n_assigned_option == capacity
+                    println("option full!")
+                    continue
+                end
+            catch e
+                print("A is empty, ")
+                global n_assigned_option = 0
             end
-            # Collect list of candidates for current option
+
+            # Now collect candidates
             is_candidate = []
-            for group in keys(I)
-                # Skip this group if it has been assigned
-                if haskey(A, group)
+            for (group, name) in enumerate(G)
+                # Skip if assigned
+                if name in keys(A)
                     push!(is_candidate, 0)
                     continue
                 end
-                value = I[group][rank][mech_step]
-                if value == 0
-                    push!(is_candidate, 0)
-                    merge!(A, Dict(group => 0))
-                else
-                    push!(is_candidate, Int(value == option))
-                end
+                # Else extract choice and add candidate if choice is equal to current option
+                choice = P[group, step]
+                push!(is_candidate, Int(choice == option - 1))
             end
-            candidates = findall(x -> x==1, is_candidate)
 
+            # Collect candidates for this option as indices
+            candidates = findall(x -> x == 1, is_candidate)
+            print("candidates: $candidates, ")
+            
             # Skip current option if no candidates
             if length(candidates) == 0
+                print("\n")
                 continue
             end
+            
+            # Select candidates up to capacity
+            remaining_capacity = capacity - n_assigned_option
+            print("remaining capacity: $remaining_capacity, ")
+            
+            if length(candidates) <= remaining_capacity
+                chosen = candidates 
+            else
+                # chosen = candidates[rand(1:length(candidates))]
+                chosen = sample(candidates, Int(remaining_capacity), replace = false)
+            end
+                print("chosen: $chosen, ")
 
-            # Choose one group randomly if there are several candidates
-            chosen = candidates[rand(1:length(candidates))]
-            merge!(A, Dict(keys(I)[chosen] => option))
+            # Add to A if topic not in A
+            for each in chosen
+                merge!(A, Dict(G[each] => option - 1))
+            end
+            println("A now is: $A\n")
         end
+        print("######################\n\n\n")
     end
-    return A
+    return(A)
 end
 
-# Run algorithm for topics
-topics = boston_mechanism(I0, C0, 1);
+# Set parameters
+Random.seed!(511)
+G1 = ("γ", "τ", "ι", "ϕ", "δ", "ω", "θ", "λ")
+C1 = [5 1 1 1 1 1 1]
+P1 = [
+    3 4 1 2 6 0 5; # γ
+    1 3 4 0 5 6 2; # τ
+    6 0 5 1 3 4 2; # ι
+    0 4 5 1 3 2 6; # ϕ
+    6 1 2 3 5 4 0; # δ
+    0 4 6 5 1 3 2; # ω
+    1 6 4 3 5 2 0; # θ
+    6 5 1 4 2 3 0 # λ
+]
 
-X = filter(p -> last(p) != 0, topics)
 
-# Run algorithm for dates
-I1 = (
-    γ = γ,
-    τ = τ,
-    λ = λ,
-    δ = δ
-)
-dates = boston_mechanism(I1, (1:3), 2);
+topics = boston_mechanism(G1, C1, P1);
+
+
+### Dates
+
+C2 = [Inf 1 1 1]
+
+## Filter set of groups to select only groups with an assigned topic
+
+# Find indices of selected groups
+selected_groups = keys(filter(p -> last(p) != 0, topics))
+
+# This is not great...
+selected_indices = []
+for (group, name) in enumerate(selected_groups)
+    push!(selected_indices, findfirst(x -> x == name, G1))
+end
+
+# Filter groups (never use tuples again!)
+v = [G1...]
+G_selected = v[filter(x -> (x in selected_indices), axes(v, 1))]
+G_selected = (G_selected...,)
+
+
+
+# Filter date preferences for selected groups only
+P_selected = [
+    2 3 1 0; # γ
+    3 2 1 0; # τ
+    3 2 1 0; # ι
+    3 2 1 0; # ϕ
+    3 2 1 0; # δ
+    3 2 1 0; # ω
+    1 2 3 0; # θ
+    1 2 3 0 # λ
+]
+P_selected = P_selected[filter(x -> (x in selected_indices), axes(P_selected, 1)), axes(P_selected, 2)]
+
+# P_selected = 
+# 2  3  1  0   "γ" *
+# 3  2  1  0   "δ" *
+# 3  2  1  0   "ω"
+# 1  2  3  0   "θ"
+# 1  2  3  0   "λ" *
+
+dates = boston_mechanism(G_selected, C2, P_selected)
+
+
+### Discussants
+
+C3 = [2 1 1 1]
+
+# Find indices of discussant groups
+# discuss_groups = keys(filter(p -> last(p) == 0, topics))
+G_discuss = ("τ", "ι", "ϕ", "ω", "θ")
+discuss_indices = [2, 3, 4, 6, 7]
+P_discuss = P1[filter(x -> (x in discuss_indices), axes(P1, 1)), axes(P1, 2)]
+
+discussants = boston_mechanism(G_discuss, C3, P_discuss)
